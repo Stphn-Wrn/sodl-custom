3dd037ae68cd82028cc78806379c198476e8d749
Stephen <stephen.woirin@optidigital.com>
2026-09-13 15:22:15 +0200

    chore: update fortune

--- scripts/config.js
+++ scripts/config.js
@@ -8,15 +8,9 @@ export const SODL_CONFIG = {
   },
 
   chancePointsRules: {
-    reserve: "Réserve de 1d3+3 points par aventure pour le groupe.",
-    gains: "Également gagnés par des actions remarquables, de bonnes idées, une bonne interprétation...",
+    gains: "Les points de chance peuvent être gagnés par des actions remarquables, de bonnes idées, une bonne interprétation et d'autres comportements méritoires...",
 
     intro: "La Fortune est faite pour être utilisée souvent. C'est à la fois un parachute en cas de situation horrible et une ressource inutile si on la garde sans s'en servir. Le livre de base définit les effets de base (résumés ici) :",
-    baseUses: [
-      "Transformer un échec en succès pour le joueur qui la dépense.",
-      "Accorder 2 atouts au jet d'attaque ou de défi d'un autre joueur.",
-      "Remplacer le résultat de n'importe quel d6 par un 6."
-    ],
 
     extendedIntro: "Ces options de base couvrent la plupart des situations (transformer un coup raté en coup au but, encourager un allié, ou arracher un ami à l'agonie en maximisant un soin ou des dégâts). Mais la Fortune donne un contrôle momentané sur l'histoire, et ses usages peuvent être élargis. Voici de nouvelles manières de dépenser la Fortune pour élargir ses pouvoirs :",
     extendedUses: [
